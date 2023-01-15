c51c73ca5c1456a446b42ff54ef7cd5f6846f80c
Lyes <lyes.dabla@gmail.com>
2023-01-15 16:29:14 +0100

    azetfrugaeioutgaez

--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -5,29 +5,60 @@ const Searchbar = ({ search }) => {
 
 
     const [searchText, setSearchText] = useState('')
-    const [searchType, setSearchType] = useState('');
+    const [searchType, setSearchType] = useState('')
+    const [error, setError] = useState(false);
     const [searchData, SetsearchData] = useState({
         searchType: searchType,
         searchText: searchText,
     });
+
+    const handleType = (e) => {
+        setSearchType(e.target.value);
+    }
+    
+    const handleText = (e) => {
+        let str = e.target.value.split(',')
+        setSearchText(str)
+    }
+    const handleForm = (e) => {
+        if (!searchText || !searchType) {
+            setError(true)
+            return;
+        } else {
+            setError(false);
+        }
+        SetsearchData({
+            searchType: searchType,
+            searchText: searchText.map((val) => (val.trim()))
+        })
+        console.log(searchData.searchText);
+        search(searchData);
+    }
     return (
         <div className="searchBar">
+
             <div className="searchField">
-
-            <input type="text" name="searchBar" value={searchText} onChange={(e) => {setSearchText(e.target.value)}} />
+                <input type="text" name="searchText" id="searchText" value={searchText} onChange={handleText} />
             </div>
+
             <div className="radios">
-                <div className="radioGroup">
-                    <input type="radio" name="searchType" value="recipe" id="" />
-                   <p>Search by Recipe</p>
-                </div>
-                <div className="radioGroup">
-                    <input type="radio" name="searchType"  value="ingridient" id="" />
-                    <p>Search by ingridient</p>
-                </div>
-            </div>
-            <p className='searchBtn' onClick={() => { search(searchText) }}> 🔎</p>
 
+                <div className="radioGroup">
+                    <label htmlFor="searchType"> Search by recipe</label>
+                    <input onChange={handleType} type="radio" name="searchType" value="recipe" id="" />
+                </div>
+
+                <div className="radioGroup">
+                    <label htmlFor="searchType"> Search by ingridient </label>
+                    <input onChange={handleType} type="radio" name="searchType"  value="ingridient" id="" />
+                </div>
+
+            </div>
+            
+            <p className='searchBtn' onClick={handleForm}> 🔎</p>
+
+
+            {error &&  <p> please fill all the fields </p> }
         </div>
     );
 }
