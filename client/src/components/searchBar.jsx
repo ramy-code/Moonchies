1d41a0b3d0bd3e4cc0f98f063f7d26462cce05d4
Lyes <lyes.dabla@gmail.com>
2023-01-15 14:40:17 +0100

    fixing the form, not done yet

--- client/src/components/searchBar.jsx
+++ client/src/components/searchBar.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 
 const Searchbar = ({ search }) => {
+
+
     const [searchText, setSearchText] = useState('')
-    useEffect(() => {
-        setSearchText(searchText)
-    }, [searchText]);
+    const [searchType, setSearchType] = useState('');
+    const [searchData, SetsearchData] = useState({
+        searchType: searchType,
+        searchText: searchText,
+    });
     return (
         <div className="searchBar">
+            <div className="searchField">
+
             <input type="text" name="searchBar" value={searchText} onChange={(e) => {setSearchText(e.target.value)}} />
+            </div>
+            <div className="radios">
+                <div className="radioGroup">
+                    <input type="radio" name="searchType" value="recipe" id="" />
+                   <p>Search by Recipe</p>
+                </div>
+                <div className="radioGroup">
+                    <input type="radio" name="searchType"  value="ingridient" id="" />
+                    <p>Search by ingridient</p>
+                </div>
+            </div>
             <p className='searchBtn' onClick={() => { search(searchText) }}> 🔎</p>
+
         </div>
     );
 }
--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 import express from "express";
-
 const app = express();
+const PORT = 5000
 
 
-app.get('./', (req,res) => {
-    
+app.post('/api', (req,res) => {
+    res.json({ "lets": "go" })
+    console.log('got a req', req.body);
 })
 
 
 
-app.listen(3000,()=>(console.log('liestinng on 3000')))
+app.listen(PORT,()=>(console.log('Listening on port: ' + PORT)))
