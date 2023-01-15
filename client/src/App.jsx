7b81875c6dd52effdcc36a94e10c54dcd59dfa4d
Lyes <lyes.dabla@gmail.com>
2023-01-15 16:36:22 +0100

    azetfrugaeioutgaez

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import Searchbar from './components/searchBar';
+import { useState,useEffect } from 'react';
+import axios from 'axios'
+function App() {
+  const [ingridients, setIngridients] = useState([])
+  const [recipe, setRecipe] = useState({});
+  const search = (obj) => {
+    setIngridients(obj);
+  }
+
+  const getRecipes = async (ingridientsArray) => {
+    axios('http://localhost:5000/api')
+    .then(res => setRecipe(res))
+    .catch(err=> console.error(err))
+  }
+
+
+  console.log(ingridients);
+
+  return (
+    <div className="App">
+      <Searchbar search={search} />
+    </div>
+  );
+}
+
+export default App;
