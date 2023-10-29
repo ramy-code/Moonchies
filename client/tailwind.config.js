85a765762876ad0a5e3edd1f65e230321a152850
Lyes <lyes.dabla@gmail.com>
2023-10-29 21:28:20 +0100

    frontend redesign

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+	theme: {
+		colors: {
+			main: "#64BAAA",
+			secondary: "#64BAAA",
+			bgColor: "#FBEAFF",
+			cta: "#64BAAA",
+		},
+	},
+	plugins: [],
+};
